d799e4681bf9ba4b0d6c5ffbcee977d40f76a566
David Sheets <sheets@alum.mit.edu>
2013-08-08 17:28:12 +0100

    Update Dnscurve_processor to enable protocol/processor composition

--- lwt/dnscurve_processor.ml
+++ lwt/dnscurve_processor.ml
@@ -1,73 +1,106 @@
 open Dns_server
 open Dnscurve
 
-module Dnscurve_protocol_only = struct
-  type txn = Streamlined of channel | Txt of channel * Dns.Packet.t
-  type 'a ctxt = 'a * Dns.Packet.t
-  type context = txn ctxt
+let between inside outside sk =
+  let module O = (val outside : PROTOCOL) in
+  let module I = (val inside : PROTOCOL) in
+  let module M = struct
+    type txn =
+    | Streamlined of I.context * channel
+    | Txt of I.context * channel * Dns.Packet.t * O.context
+    type 'a ctxt = 'a * Dns.Packet.t
+    type context = txn ctxt
 
-  let query_of_context = snd
+    let query_of_context = snd
 
-  let parse sk buf =
-    try
-      let chan, pkt = decode_streamlined_query sk buf in
-      Some (Streamlined chan, pkt)
-    with Protocol_error _ -> begin
+    let parse buf =
       try
-        let txt_wrapper = Dns.Packet.parse buf in
-        let chan, pkt = decode_txt_query sk txt_wrapper in
-        Some (Txt (chan, txt_wrapper), pkt)
-      with Protocol_error _ | Base32.Decode_error _ -> None (* TODO: log *)
+        let chan, buf = decode_streamlined_query sk buf in
+        begin match I.parse buf with
+        | Some ictxt ->
+          Some (Streamlined (ictxt, chan), I.query_of_context ictxt)
+        | None -> None (* TODO: log *)
+        end
+      with Protocol_error _ -> begin
+        try begin match O.parse buf with
+        | Some octxt ->
+          let txt = O.query_of_context octxt in
+          let chan, buf = decode_txt_query sk txt in
+          begin match I.parse buf with
+          | Some ictxt ->
+            Some (Txt (ictxt, chan, txt, octxt), I.query_of_context ictxt)
+          | None -> prerr_endline "inner parse fail\n"; None (* TODO: log *)
+          end
+        | None -> prerr_endline "outer parse fail\n"; None (* TODO: log *)
+        end
+        with (Protocol_error _ | Base32.Decode_error _) as exn ->
+          Printf.eprintf "Exception: %s\nBacktrace:\n%!" (Printexc.to_string exn);
+          Printexc.print_backtrace stderr;
+          None (* TODO: log *)
+        | exn ->
+          Printf.eprintf "Unknown exception: %s\nBacktrace:\n%!" (Printexc.to_string exn);
+          Printexc.print_backtrace stderr;
+          None (* TODO: log *)
+      end
       | _exn -> None (* TODO: log *)
-    end
-    | _exn -> None (* TODO: log *)
 
-  let marshal buf (txn, _) pkt = Some (match txn with
-    | Streamlined chan -> encode_streamlined_response chan pkt
-    | Txt (chan, query) ->
-      Dns.Packet.marshal buf (encode_txt_response chan query pkt)
-  )
-end
+    let marshal buf (txn, _) pkt = match txn with
+      | Streamlined (ictxt, chan) ->
+        begin match I.marshal buf ictxt pkt with
+        | Some buf -> Some (encode_streamlined_response chan buf)
+        | None -> None (* TODO: log *)
+        end
+      | Txt (ictxt, chan, txt, octxt) ->
+        begin match I.marshal buf ictxt pkt with
+        | Some buf ->
+          let pkt = encode_txt_response chan txt buf in
+          let obuf = Dns.Buf.create 4096 in
+          O.marshal obuf octxt pkt
+        | None -> None (* TODO: log *)
+        end
+  end in
+  (module M : PROTOCOL)
 
-module Dnscurve_protocol = struct
-  module P = Dnscurve_protocol_only
-  type context = P.txn option P.ctxt
+let fallback protocol sk clear_process encrypt_process =
+  let module D = (val protocol : PROTOCOL) in
+  let module P = (val (between protocol protocol sk) : PROTOCOL) in
+  let module M = struct
+    type context = Curve of P.context | Clear of D.context
 
-  let query_of_context = P.query_of_context
+    let query_of_context = function
+      | Curve c -> P.query_of_context c
+      | Clear c -> D.query_of_context c
 
-  let parse sk buf = match P.parse sk buf with
-    | Some (chan, pkt) -> Some (Some chan, pkt)
-    | None -> begin
-      try Some (None, Dns.Packet.parse buf)
-      with _ -> prerr_endline "bad clear DNS query"; None (* TODO: log *)
-    end
+    let parse buf = match P.parse buf with
+      | Some ctxt -> Some (Curve ctxt)
+      | None -> begin match D.parse buf with
+        | Some ctxt -> Some (Clear ctxt)
+        | None -> prerr_endline "bad clear DNS query"; None (* TODO: log *)
+      end
 
-  let marshal buf (otxn, _) pkt = Some (match otxn with
-    | Some (P.Streamlined chan) -> encode_streamlined_response chan pkt
-    | Some (P.Txt (chan, query)) ->
-      Dns.Packet.marshal buf (encode_txt_response chan query pkt)
-    | None -> Dns.Packet.marshal buf pkt
-  )
-end
+    let process ~src ~dst ctxt =
+      (match ctxt with
+      | Curve _ -> encrypt_process
+      | Clear _ -> clear_process) ~src ~dst (query_of_context ctxt)
+
+    let marshal buf ctxt pkt = match ctxt with
+      | Curve c -> P.marshal buf c pkt
+      | Clear c -> D.marshal buf c pkt
+  end in
+  (module M : PROCESSOR)
 
 let secure_of_process sk process =
+  let dns = (module Dns_protocol : PROTOCOL) in
+  let module C = (val (between dns dns sk) : PROTOCOL) in
   let module P = struct
-    include Dnscurve_protocol_only
+    include C
 
-    let parse = parse sk
-    let process ~src ~dst (_,pkt) = process ~src ~dst pkt
+    let process ~src ~dst ctxt = process ~src ~dst (C.query_of_context ctxt)
   end in
   (module P : PROCESSOR)
 
 let split_of_process sk clear_process encrypt_process =
-  let module P = struct
-    include Dnscurve_protocol
-
-    let parse = parse sk
-    let process ~src ~dst (chan,pkt) =
-      (match chan with Some _ -> encrypt_process | None -> clear_process)
-        ~src ~dst pkt
-  end in
-  (module P : PROCESSOR)
+  let dns = (module Dns_protocol : PROTOCOL) in
+  fallback dns sk clear_process encrypt_process
 
 let fallback_of_process sk process = split_of_process sk process process
--- lwt/dnscurve_processor.mli
+++ lwt/dnscurve_processor.mli
@@ -1,36 +1,21 @@
-module Dnscurve_protocol_only : sig
-  type txn =
-    Streamlined of Dnscurve.channel
-  | Txt of Dnscurve.channel * Dns.Packet.t
-  type 'a ctxt = 'a * Dns.Packet.t
-  type context = txn ctxt
-  val query_of_context : context -> Dns.Packet.t
-  val parse :
-    Sodium.secret Sodium.Box.key ->
-    Sodium.Serialize.Bigarray.t -> context option
-  val marshal :
-    Dns.Buf.t ->
-    context -> Dns.Packet.t -> Sodium.Serialize.Bigarray.t option
-end
+open Dns_server
+open Sodium
 
-module Dnscurve_protocol : sig
-  type context = Dnscurve_protocol_only.txn option Dnscurve_protocol_only.ctxt
-  val query_of_context : context -> Dns.Packet.t
-  val parse :
-    Sodium.secret Sodium.Box.key ->
-    Sodium.Serialize.Bigarray.t -> context option
-  val marshal :
-    Dns.Buf.t ->
-    context -> Dns.Packet.t -> Sodium.Serialize.Bigarray.t option
-end
+val between :
+  (module PROTOCOL) -> (module PROTOCOL) -> secret Box.key ->
+  (module PROTOCOL)
+
+val fallback :
+  (module PROTOCOL) -> secret Box.key ->
+  Dns.Packet.t process -> Dns.Packet.t process ->
+  (module PROCESSOR)
 
 val secure_of_process :
-  Sodium.secret Sodium.Box.key ->
-  Dns.Packet.t Dns_server.process -> (module Dns_server.PROCESSOR)
+  secret Box.key -> Dns.Packet.t process -> (module Dns_server.PROCESSOR)
+
 val split_of_process :
-  Sodium.secret Sodium.Box.key ->
-  Dns.Packet.t Dns_server.process ->
-  Dns.Packet.t Dns_server.process -> (module Dns_server.PROCESSOR)
+  secret Box.key -> Dns.Packet.t process -> Dns.Packet.t process ->
+  (module PROCESSOR)
+
 val fallback_of_process :
-  Sodium.secret Sodium.Box.key ->
-  Dns.Packet.t Dns_server.process -> (module Dns_server.PROCESSOR)
+  secret Box.key -> Dns.Packet.t process -> (module PROCESSOR)
