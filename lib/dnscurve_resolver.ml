888a29c7c603a2426bf39c04bd4995330354af0a
David Sheets <sheets@alum.mit.edu>
2013-08-16 21:46:49 -0400

    Add streamlined DNSCURVECLIENT for only that mode

--- lib/dnscurve_resolver.ml
+++ lib/dnscurve_resolver.ml
@@ -5,30 +5,61 @@ module Crypto = Sodium.Make(Sodium.Serialize.String)
 
 type env = { mutable streamlined : bool option; mutable txt : bool option; }
 
+module type DNSCURVECLIENT = sig
+  include CLIENT
+
+  val marshal : keyring option -> Sodium.Box.keypair -> Dns.Packet.t ->
+    (context * Dns.Buf.t) list
+end
+
 let new_env () = { streamlined = None; txt = None; }
 let reset_env env = env.streamlined <- None; env.txt <- None; ()
+let get_id () =
+  let s = Crypto.random 2 in
+  ((int_of_char s.[0]) lsl 8) lor (int_of_char s.[1])
+
+let streamlined server_pk inside =
+  let module I = (val inside : CLIENT) in
+  let module M = struct
+    type context = I.context * channel
+
+    let get_id = get_id
+
+    let marshal keyring ident pkt =
+      List.rev_map
+        (fun (ictxt, buf) ->
+          let chan, buf = encode_streamlined_query ?keyring ident
+            server_pk buf in
+          (ictxt, chan), buf)
+        (I.marshal pkt)
+
+    let parse (ictxt, chan) buf =
+      try I.parse ictxt (decode_streamlined_response chan buf)
+      with exn ->
+        Printf.eprintf "Exception: %s\nBacktrace:\n%!" (Printexc.to_string exn);
+        Printexc.print_backtrace stderr;
+        None (* TODO: tag context for timeout callback *)
+
+    let timeout _ = Dns_resolve_timeout
+  end in
+  (module M : DNSCURVECLIENT)
 
 let between keyf env server_pk zone outside inside =
   let module O = (val outside : CLIENT) in
   let module I = (val inside : CLIENT) in
+  let module S = (val streamlined server_pk inside : DNSCURVECLIENT) in
   let module M = struct
     type context =
-    | Streamlined of env * I.context * channel
+    | Streamlined of env * S.context
     | Txt of env * I.context * channel * O.context
 
-    let get_id () =
-      let s = Crypto.random 2 in
-      ((int_of_char s.[0]) lsl 8) lor (int_of_char s.[1])
+    let get_id = get_id
 
     let marshal pkt =
       let keyring, ident = keyf () in
       let streamlined () =
-        List.rev_map
-          (fun (ictxt, buf) ->
-            let chan, buf = encode_streamlined_query ?keyring ident
-              server_pk buf in
-            Streamlined (env, ictxt, chan), buf)
-          (I.marshal pkt)
+        List.rev_map (fun (sctxt, buf) ->
+          Streamlined (env, sctxt), buf) (S.marshal keyring ident pkt)
       in
       let txt () =
         let id = get_id () in
@@ -49,8 +80,8 @@ let between keyf env server_pk zone outside inside =
       | { streamlined=Some false; txt=Some false } -> []
 
     let parse chan buf = try begin match chan with
-      | Streamlined (env,ictxt,chan) ->
-        let pkt = I.parse ictxt (decode_streamlined_response chan buf) in
+      | Streamlined (env,sctxt) ->
+        let pkt = S.parse sctxt buf in
         env.streamlined <- Some true;
         pkt
       | Txt (env,ictxt,chan,octxt) ->
@@ -67,7 +98,7 @@ let between keyf env server_pk zone outside inside =
       None (* TODO: tag context for timeout callback *)
 
     let timeout = function
-      | Streamlined (env,_,_) ->
+      | Streamlined (env,_) ->
         env.streamlined <- Some false; Dns_resolve_timeout
       | Txt (env,_,_,_) ->
         env.txt <- Some false; Dns_resolve_timeout
--- lib/dnscurve_resolver.mli
+++ lib/dnscurve_resolver.mli
@@ -2,8 +2,18 @@ open Sodium
 
 type env = { mutable streamlined : bool option; mutable txt : bool option; }
 
+module type DNSCURVECLIENT = sig
+  include Dns.Protocol.CLIENT
+
+  val marshal : Dnscurve.keyring option -> Sodium.Box.keypair -> Dns.Packet.t ->
+    (context * Dns.Buf.t) list
+end
+
 val new_env : unit -> env
 val reset_env : env -> unit
+
+val streamlined : public Box.key -> (module Dns.Protocol.CLIENT) ->
+  (module DNSCURVECLIENT)
 
 val between :
   (unit -> Dnscurve.keyring option * Box.keypair) ->
